a3141562ffd3fd1a214bc4ca2f18a5668d7e1ba8
vincent schelstraete <schelstraetevincent2025@serrahs.com>
2025-04-30 12:16:33 -0700

    recent changes

--- app/src/main/java/com/example/scheduleapp/TaskFragment.java
+++ app/src/main/java/com/example/scheduleapp/TaskFragment.java
@@ -38,6 +38,7 @@ public class TaskFragment extends Fragment {
     private Button dueTimeButton;
     private EditText timeNeeded;
     private CheckBox isCompleted;
+    private CheckBox isAlternating;
 
     public static TaskFragment newInstance(UUID taskID) {
         Bundle args = new Bundle();
@@ -127,6 +128,9 @@ public class TaskFragment extends Fragment {
         isCompleted.setChecked(task.isCompleted());
         isCompleted.setOnCheckedChangeListener((buttonView, isChecked) -> task.setCompleted(isChecked));
 
+        isAlternating = v.findViewById(R.id.is_alternating);
+        isAlternating.setChecked(task.isAlternating());
+        isAlternating.setOnCheckedChangeListener((buttonView, isChecked) -> task.setAlternating(isChecked));
         return v;
     }
 
--- app/src/main/java/com/example/scheduleapp/TaskListFragment.java
+++ app/src/main/java/com/example/scheduleapp/TaskListFragment.java
@@ -88,6 +88,7 @@ public class TaskListFragment extends Fragment {
         private TextView mDueDateTextView;
         private Button mStartButton;
         private CheckBox mSolvedCheckBox;
+        private CheckBox mAlternating;
         private Task mTask;
 
         public TaskHolder(LayoutInflater inflater, ViewGroup parent) {
@@ -98,6 +99,7 @@ public class TaskListFragment extends Fragment {
             mDueDateTextView = itemView.findViewById(R.id.task_due_date);
             mStartButton = itemView.findViewById(R.id.start_task);
             mSolvedCheckBox = itemView.findViewById(R.id.is_completed);
+            mAlternating = itemView.findViewById(R.id.is_alternating);
         }
 
         public void bind(Task task) {
@@ -107,7 +109,9 @@ public class TaskListFragment extends Fragment {
             mDueDateTextView.setText(mTask.getDueTimeString());
             mStartButton.setOnClickListener(v -> startTask());
             mSolvedCheckBox.setChecked(task.isCompleted());
-
+            if (mAlternating != null) {
+                mAlternating.setChecked(task.isAlternating());
+            }
             updateStrikeThrough(task.isCompleted());
 
             updateBackgroundColor(task.isCompleted());
@@ -117,6 +121,12 @@ public class TaskListFragment extends Fragment {
                 updateStrikeThrough(isChecked);
                 updateBackgroundColor(isChecked);
             });
+
+            mAlternating.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                mTask.setAlternating(isChecked);
+            });
+
+
         }
 
         private void updateBackgroundColor(boolean isChecked) {
